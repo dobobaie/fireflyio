816a526e663b7bf93ae868e590cfcee6f8108371
dobobaie <dobobaie@hotmail.fr>
2020-01-22 15:10:18 +0100

    Feature(Service): convert event model to socket model

--- tests/extend.test.js
+++ tests/extend.test.js
@@ -5,7 +5,10 @@ const FireflyioMonitoring = require('../../fireflyio-monitoring/lib');
   // ---
   const app = new Fireflyio({ debug: true });
   app.extend(FireflyioMonitoring, {
-    debug: true
+    debug: true,
+    maxNumberServers: 2,
+    maxNumberUsersPerServer: 1,
+    isLocal: true
   });
   app.listen(2525);
   // ---
