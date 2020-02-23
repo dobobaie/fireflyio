7172d1d81f118df4609db91f36b25938c74f2222
dobobaie <dobobaie@hotmail.fr>
2020-02-23 13:02:15 +0700

    Fix(Service): fix middleware no async socket

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -4,5 +4,12 @@ const app = new Fireflyio({ debug: true, allowedHttpRequests: true });
 app.use(ctx => {
   ctx.body = 'Hello Fireflyio';
 });
- 
+
+app.socket.on("clientConnected", client =>
+  client.emit("HELLO_CLIENT", (_, next) => {
+    // everything is ok
+    next();
+  }, 'Hello Fireflyio')
+);
+
 app.listen(4000);
